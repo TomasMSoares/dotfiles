b7d06e8af751fb926dba98631c2fe6f30092c6b4
t-soares-reply <t.soares@reply.de>
2026-06-15 10:28:32 +0200

    Add lazygit and lazygit.nvim with basic config

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -18,5 +18,22 @@ return {
         current_line_blame = true,
       })
     end
-  }
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gi", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 }
