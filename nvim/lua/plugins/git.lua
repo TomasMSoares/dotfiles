053de39ace33600424252adfcd7ff995a05c6e7d
t-soares-reply <t.soares@reply.de>
2026-06-15 14:51:55 +0200

    Better lazygit bindings

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,7 +2,8 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup({
+      local gs = require('gitsigns')
+      gs.setup({
         signs = {
           add          = { text = '┃' },
           change       = { text = '┃' },
@@ -11,11 +12,33 @@ return {
           changedelete = { text = '~' },
           untracked    = { text = '┆' },
         },
-        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        signcolumn = true,
         current_line_blame = true,
+        on_attach = function(bufnr)
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']h', function()
+            if vim.wo.diff then return ']h' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true, desc = "Next Hunk" })
+
+          map('n', '[h', function()
+            if vim.wo.diff then return '[h' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true, desc = "Prev Hunk" })
+
+          -- Actions (Moved to <leader>g prefix to avoid collision with <leader>h terminal)
+          map('n', '<leader>gs', gs.stage_hunk, { desc = "Stage Hunk" })
+          map('n', '<leader>gp', gs.preview_hunk, { desc = "Preview Hunk" })
+          map('n', '<leader>gr', gs.reset_hunk, { desc = "Reset Hunk" })
+        end
       })
     end
   },
@@ -33,7 +56,19 @@ return {
       "nvim-lua/plenary.nvim",
     },
     keys = {
-      { "<leader>gi", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { 
+        "<leader>gg", 
+        function()
+          -- If we are currently in a lazygit buffer, close it
+          if vim.bo.filetype == "lazygit" then
+            vim.cmd("close")
+          else
+            vim.cmd("LazyGit")
+          end
+        end, 
+        desc = "Toggle LazyGit",
+        mode = { "n", "t" } -- Allow toggling from both Normal and Terminal mode
+      },
     },
   },
 }
